7247d9780fab28f683defa3ed37fc7d5f2026e57
zchyx <93565914+zchyx@users.noreply.github.com>
2021-11-10 16:36:05 +0800

    Create 3-12.c

---
+++ 3-12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int no;
+  printf("请输入一个整数：");
+  scanf("%d",&no);
+  switch(no%2)
+  {
+    case 0:puts("该整数是偶数。");break;
+    case 1:
+    case -1:puts("该整数是奇数。");break;
+  }
+  return 0;
+}
