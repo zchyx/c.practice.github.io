3e8a1911ad9b12dcb1810e5e0530ff9ef019c8ea
zchyx <93565914+zchyx@users.noreply.github.com>
2021-11-15 15:16:51 +0800

    Create 4-6.c

---
+++ 4-6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  int a,i=0;
+  printf("请输入一个整数：");
+  scanf("%d",&a);
+  i%2==0;
+  while(i<a-2)
+  {
+    printf("%d ",i=i+2);}
+  printf("\n");
+  return 0;
+}
