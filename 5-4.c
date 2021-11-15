1c8cb2c16b66c5cb942d887a86f88117d48fa09d
zchyx <93565914+zchyx@users.noreply.github.com>
2021-11-15 23:07:53 +0800

    Create 5-4.c

---
+++ 5-4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int i,a[5]={17,23,36},b[5];
+  for(i=0;i<5;i++)
+    b[i]=a[4-i];
+  puts("  a    b");
+  puts("-----------");
+  for(i=0;i<5;i++)
+    printf("%4d%4d\n",a[i],b[i]);
+  return 0;
+}
